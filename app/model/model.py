cfbb31c1542f6d1d30351b05f63d235e6e230c9d
alllpina <danilcrystal8@gmail.com>
2025-01-04 01:45:42 +0200

    model v1 completed container

--- app/model/model.py
+++ app/model/model.py
@@ -1,37 +1,3 @@
-# from flask import Flask, request, jsonify
-# import numpy as np
-# import pickle
-
-# with open('./models/random_forest_model_1.pkl', 'rb') as mf: # not motherf#ucker, mf - model file
-#     model = pickle.load(mf)
-
-# with open('./models/scaler1.pkl', 'rb') as sf: # sf - sucker file, jk it's scaler file :P
-#     scaler = pickle.load(sf)
-
-# def data_preprocess(data):
-#     data = data['features'][0]
-#     type_of_work = data[-1]
-#     data_to_scale = data[:-1]
-#     scaled_data = scaler.transform([data_to_scale])
-#     data = np.append(scaled_data, type_of_work)
-#     return data
-
-# def make_prediction(data):
-#     data = data_preprocess(data)
-#     prediction = model.predict([data])
-#     return prediction
-
-# app = Flask(__name__)
-
-# @app.route('/predict', methods=['POST'])
-# def predict():
-#     prediction = make_prediction(request.json)
-    
-#     return jsonify(predictions=prediction.tolist())
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
 from flask import Flask, request, jsonify
 from utilities.utils import make_prediction
 import pickle
@@ -70,4 +36,4 @@ def predict():
         return jsonify({'error': 'An unexpected error occurred'}), 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5000)
